c78a6fd599018e721e91ed8d823907dc3280bd33
Simon Cook <simon.cook@salesforce.com>
2016-10-17 23:01:29 +0100

    Added Web2Case Form

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/webtocase', function(request, response) {
+  response.render('pages/webtocase');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
